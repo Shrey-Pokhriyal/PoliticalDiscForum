b2c420b6e45859da56ab71e812b2f781f0276f05
Shrey-Pokhriyal <mail2mr.shreypokhriyal@gmail.com>
2022-04-20 20:37:03 +0530

    "dirs"

--- server/Routes/searchRoutes.js
+++ server/Routes/searchRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const search = require('../../controllers/searchController');
+const search = require('../controllers/searchController');
 
 router.get('/search', search);
 
