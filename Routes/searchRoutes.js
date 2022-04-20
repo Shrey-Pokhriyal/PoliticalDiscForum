e4fdecf320805263ee8764114c66a2b97c04ac80
Shrey Pokhriyal <89126760+Shrey-Pokhriyal@users.noreply.github.com>
2022-04-20 20:38:09 +0530

    Delete searchRoutes.js

--- Routes/searchRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const search = require('../../controllers/searchController');
-
-router.get('/search', search);
-
-module.exports = router;
